6695edc9619d47067d293b8aac6e691c206a7858
Lea732 <leapoulin07@gmail.com>
2024-02-28 15:20:19 +0100

    Added projectile launch from spaceship

--- player.py
+++ player.py
@@ -1,19 +1,27 @@
 import pygame
+from projectile import Projectile
 
+# Classe représentant notre joueur (le vaisseau allié)
 class Player(pygame.sprite.Sprite):
     
+    # définition des propriétés du joueur
     def __init__(self):
         super().__init__()
         self.health = 100
         self.max_health = 100
         self.attack = 10
         self.velocity = 5
+        self.all_projectiles = pygame.sprite.Group()
         self.original_image = pygame.image.load('assets/spaceship_allier.png')
         self.image = pygame.transform.scale(self.original_image, (200, 150))
         self.rect = self.image.get_rect()
         self.rect.x = 400
         self.rect.y = 400
-        
+    
+    # méthodes propres à la classe du joueur
+    def launch_projectile(self):
+        self.all_projectiles.add(Projectile(self))
+
     def move_right(self):
         self.rect.x += self.velocity
         
---
+++ projectile.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.sprite import Group
+
+# Classe représentant un projectile
+class Projectile(pygame.sprite.Sprite):
+    
+    def __init__(self, player):
+        super().__init__()
+        self.velocity = 10
+        self.player = player
+        self.image = pygame.image.load('assets/projectile_allier.png')
+        self.image = pygame.transform.scale(self.image, (100, 70))
+        self.rect = self.image.get_rect()
+        self.rect.x = player.rect.x + 120
+        self.rect.y = player.rect.y + 40
+        
+    def remove_shoots(self):
+        #supprimer le projectile (en dehors de l'écran)
+        self.player.all_projectiles.remove(self)
+
+    def move(self):
+        self.rect.x += self.velocity
+
+        if self.rect.x > 1080:
+            self.remove_shoots()
+            print("Projectile supprimé !")
